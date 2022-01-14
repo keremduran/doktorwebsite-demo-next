f46ef3f004c97612a8097f00692f28f27f732791
keremduran <keremduran@live.com>
2022-01-13 22:35:36 -0500

    Styling

--- components/Header.js
+++ components/Header.js
@@ -3,17 +3,17 @@ import React from 'react';
 export default function Header(props) {
   return (
     <header
-      className='masthead d-flex justify-content-center align-items-end'
+      className='masthead d-flex justify-content-center align-items-center'
       style={{
-        backgroundImage: 'url("assets/img/DoctorImage.jpg")',
+        backgroundImage: `url(${props.imgUrl})`,
         backgroundPosition: '50% 20%',
         height: '90vh',
       }}
     >
       <div className='overlay' />
-      <div className='container h-75 d-flex align-items-right'>
-        <div className='col-md-10 col-lg-8 offset-5 offset-sm-4 position-relative flex-column justify-content-center align-items-center'>
-          <div className='site-heading container' style={{ padding: 0 }}>
+      <div className='container h-75 d-flex justify-content-center align-items-center'>
+        <div className='col-md-10 col-lg-8 offset-sm-4 position-relative flex-column justify-content-center align-items-center'>
+          <div className='p-0 site-heading container'>
             <h1 className='mt-10'>
               {props.heading}
               {props.heading2 && (
@@ -28,9 +28,9 @@ export default function Header(props) {
             </h1>
           </div>
           {props.buttonText && (
-            <div class='continue-button container d-flex justify-content-center align-items-center'>
+            <div class='continue-button container d-flex '>
               <button
-                className='btn btn-primary m-2 data-mdb-toggle="animation" data-mdb-animation-reset="true" data-mdb-animation="slide-out-right'
+                className='btn btn-dark m-2 mx-auto data-mdb-toggle="animation" data-mdb-animation-reset="true" data-mdb-animation="slide-out-right'
                 type='button'
               >
                 {props.buttonText}
