1706ae48877033adbd8020eb165b5f9425643ea5
keremduran <keremduran@live.com>
2022-01-13 22:36:15 -0500

    Dummy data fetching

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -3,17 +3,33 @@ import MainNav from '../MainNav';
 import Footer from '../Footer';
 import HeadImports from '../HeadImports';
 import BottomImports from '../BottomImports';
+import Header from '../Header';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import { fetchApi } from '../../util/fetchApi';
 
 function Layout(props) {
+  const router = useRouter();
+  const data = fetchApi(router.pathname);
   return (
     <div>
       <HeadImports />
       <MainNav />
+      <Header {...data} />
       <main>{props.children}</main>
       <Footer />
       <BottomImports />
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  const router = useRouter();
+  const data = fetchApi(router.pathname);
+  console.log('TEST', data);
+  return {
+    data,
+  };
 }
 
 export default Layout;
---
+++ util/fetchApi.js
@@ -0,0 +1,48 @@
+const TEST_DATA = {
+  default: {
+    imgUrl: 'assets/img/DoctorImage.jpg',
+    heading: `Doktor`,
+    heading2: `Ad Soyad`,
+    subheading: 'Uzmanlik',
+    buttonText: 'Devam et',
+  },
+  about: {
+    imgUrl: 'assets/img/AboutImageMain.jpg',
+    heading: `Hakkimizda`,
+    subheading: 'Yaptiklarimiz',
+    buttonText: 'Bize Ulas',
+  },
+  contact: {
+    imgUrl: 'assets/img/contact-bg.jpg',
+    heading: `Iletisim`,
+    subheading: 'Sorularinizin cevabi burada!',
+  },
+  posts: {
+    imgUrl: 'assets/img/YenilikciHastane.jpg',
+    heading: `Yenilikci Hastane Secenekleri`,
+    subheading: 'Paylasan: Doktor Ad Soyad, Tarih: 22 Ocak 2022',
+  },
+};
+
+export const fetchApi = (url) => {
+  let props;
+  switch (url) {
+    case '/':
+      props = TEST_DATA.default;
+      break;
+    case '/about':
+      props = TEST_DATA.about;
+      break;
+    case '/contact':
+      props = TEST_DATA.contact;
+      break;
+    case '/posts':
+      props = TEST_DATA.posts;
+      break;
+    default:
+      props = TEST_DATA.default;
+      break;
+  }
+  console.log('FETCHAPI', url, props);
+  return props;
+};
