2775bacdfe18b88ca6a42e775324b2d8a62a33e9
keremduran <keremduran@live.com>
2022-01-14 00:17:37 -0500

    Bug Fix (class -> className)

--- components/Header.js
+++ components/Header.js
@@ -11,33 +11,28 @@ export default function Header(props) {
       }}
     >
       <div className='overlay' />
-      <div className='container h-75 d-flex justify-content-center align-items-center'>
-        <div className='col-md-10 col-lg-8 offset-sm-4 position-relative flex-column justify-content-center align-items-center'>
-          <div className='p-0 site-heading container'>
-            <h1 className='mt-10'>
-              {props.heading}
-              {props.heading2 && (
-                <span class='heading2'>
-                  <br />
-                  {props.heading2}
-                </span>
-              )}
-              {props.subheading && (
-                <span className='subheading'>{props.subheading}</span>
-              )}
-            </h1>
-          </div>
-          {props.buttonText && (
-            <div class='continue-button container d-flex '>
-              <button
-                className='btn btn-dark m-2 mx-auto data-mdb-toggle="animation" data-mdb-animation-reset="true" data-mdb-animation="slide-out-right'
-                type='button'
-              >
-                {props.buttonText}
-              </button>
-            </div>
-          )}
+      <div className='col-md-10 col-lg-8 offset-sm-4 position-relative flex-column justify-content-center align-items-center'>
+        <div className='p-0 site-heading container'>
+          <h1 className='mt-10'>
+            {props.heading}
+            {props.heading2 && (
+              <span className='heading2'>
+                <br />
+                {props.heading2}
+              </span>
+            )}
+            {props.subheading && (
+              <span className='subheading'>{props.subheading}</span>
+            )}
+          </h1>
         </div>
+        {props.buttonText && (
+          <div className='continue-button d-flex'>
+            <button className='btn btn-dark m-2 mx-auto' type='button'>
+              {props.buttonText}
+            </button>
+          </div>
+        )}
       </div>
     </header>
   );
