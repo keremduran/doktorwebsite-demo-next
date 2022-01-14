b34725186ca9e9056c788b74896266515ab61326
keremduran <keremduran@live.com>
2022-01-13 20:24:25 -0500

    Header generalized

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function Header() {
+export default function Header(props) {
   return (
     <header
       className='masthead d-flex justify-content-center align-items-end'
@@ -15,22 +15,28 @@ export default function Header() {
         <div className='col-md-10 col-lg-8 offset-5 offset-sm-4 position-relative flex-column justify-content-center align-items-center'>
           <div className='site-heading container' style={{ padding: 0 }}>
             <h1 className='mt-10'>
-              Doktor
-              <br />
-              Ad Soyad
-              <br />
-              <span className='subheading'>Uzmanlik</span>
+              {props.heading}
+              {props.heading2 && (
+                <span class='heading2'>
+                  <br />
+                  {props.heading2}
+                </span>
+              )}
+              {props.subheading && (
+                <span className='subheading'>{props.subheading}</span>
+              )}
             </h1>
           </div>
-          {/* Continue Button */}
-          <div class='continue-button container d-flex justify-content-center align-items-center'>
-            <button
-              className='btn btn-primary m-2 data-mdb-toggle="animation" data-mdb-animation-reset="true" data-mdb-animation="slide-out-right'
-              type='button'
-            >
-              devam et
-            </button>
-          </div>
+          {props.buttonText && (
+            <div class='continue-button container d-flex justify-content-center align-items-center'>
+              <button
+                className='btn btn-primary m-2 data-mdb-toggle="animation" data-mdb-animation-reset="true" data-mdb-animation="slide-out-right'
+                type='button'
+              >
+                {props.buttonText}
+              </button>
+            </div>
+          )}
         </div>
       </div>
     </header>
